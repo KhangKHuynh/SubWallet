7f1145887b462a2efa8019d1bcf215b9f8183b61
Khang Kyle Huynh <kkyhuynh@gmail.com>
2025-09-05 13:03:35 -0500

    added to gitignore

--- SubWallet/Models/ErrorViewModel.cs
+++ SubWallet/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace WebApplication1.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string? RequestId { get; set; } //research this 
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
